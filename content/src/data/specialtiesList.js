76f7d02675c92de4a6e91d88df38aa7fa13f0394
DanielArrivabene <mentalize.clinica10@gmail.com>
2023-10-30 20:34:57 -0300

    clear

--- content/src/data/specialtiesList.js
+++
@@ -1,9 +0,0 @@
-import Medicine from '../assets/medicina.svg';
-
-export const specialtiesList = [
-  {
-    name: 'Medicina',
-    icon: Medicine,
-    descryption: ['Saúde Mental', 'Clínica Geral', 'Estética'],
-  },
-];
